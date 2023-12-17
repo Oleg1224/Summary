2d942135b948661e24d585721311f314ba750f87
Oleg1224 <olegik.kr@gmail.com>
2023-12-17 17:40:06 +0200

    1

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ aSvg.addEventListener('click', function() {
         modal1.classList.add('dop')
         modal2.classList.remove('dop')
         modal3.classList.remove('dop')
+        modal4.classList.remove('dop')
+        modal5.classList.remove('dop')
+        modal6.classList.remove('dop')
     }
     else {
         modal1.classList.remove('dop')
@@ -15,6 +18,11 @@ let modal2 = document.querySelector('.modal2');
 aSvg2.addEventListener('click', function() {
     if (modal2.className === 'modal2') {
         modal2.classList.add('dop')
+        modal1.classList.remove('dop')
+        modal3.classList.remove('dop')
+        modal4.classList.remove('dop')
+        modal5.classList.remove('dop')
+        modal6.classList.remove('dop')
     }
     else {
         modal2.classList.remove('dop')
@@ -25,8 +33,58 @@ let modal3 = document.querySelector('.modal3');
 aSvg3.addEventListener('click', function() {
     if (modal3.className === 'modal3') {
         modal3.classList.add('dop')
+        modal1.classList.remove('dop')
+        modal2.classList.remove('dop')
+        modal4.classList.remove('dop')
+        modal5.classList.remove('dop')
+        modal6.classList.remove('dop')
     }
     else {
         modal3.classList.remove('dop')
+    }
+})
+let aSvg4 = document.querySelector('.SVG4');
+let modal4 = document.querySelector('.modal4');
+aSvg4.addEventListener('click', function() {
+    if (modal4.className === 'modal4') {
+        modal4.classList.add('dop')
+        modal1.classList.remove('dop')
+        modal2.classList.remove('dop')
+        modal3.classList.remove('dop')
+        modal5.classList.remove('dop')
+        modal6.classList.remove('dop')
+    }
+    else {
+        modal4.classList.remove('dop')
+    }
+})
+let aSvg5 = document.querySelector('.SVG5');
+let modal5 = document.querySelector('.modal5');
+aSvg5.addEventListener('click', function() {
+    if (modal5.className === 'modal5') {
+        modal5.classList.add('dop')
+        modal1.classList.remove('dop')
+        modal2.classList.remove('dop')
+        modal3.classList.remove('dop')
+        modal4.classList.remove('dop')
+        modal6.classList.remove('dop')
+    }
+    else {
+        modal5.classList.remove('dop')
+    }
+})
+let aSvg6 = document.querySelector('.SVG6');
+let modal6 = document.querySelector('.modal6');
+aSvg6.addEventListener('click', function() {
+    if (modal6.className === 'modal6') {
+        modal6.classList.add('dop')
+        modal1.classList.remove('dop')
+        modal2.classList.remove('dop')
+        modal3.classList.remove('dop')
+        modal4.classList.remove('dop')
+        modal5.classList.remove('dop')
+    }
+    else {
+        modal6.classList.remove('dop')
     }
 })
